8a12e40b246d0d8a1d5087f45525016f4dcffb39
Yuri Elias Alcântara <150107636+YuriElias07@users.noreply.github.com>
2024-04-13 13:56:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const titulo = document.querySelectorAll("")
+const autor = document.querySelectorAll("")
+const genero = document.querySelectorAll("")
+const ano = document.querySelectorAll("")
+
+
+
+const livro = {
+    Título: titulo.value,
+    Autor: autor.value,
+    Genero: genero.value,
+    Publicação: ano.value
+}
